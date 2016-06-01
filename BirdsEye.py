bb7808818c7f735335e3b34c3c26388067b286d3
bb20basketball <bb20basketball@gmail.com>
2016-06-01 17:38:22 -0500

    Update BirdsEye.py

--- BirdsEye.py
+++ BirdsEye.py
@@ -2,11 +2,11 @@ import cv2 as cv
 import numpy as np
 
 img1 = cv.imread("testBird.jpg", cv.IMREAD_COLOR)
-src = np.array([[0,0],[480,0],[480,360],[0,360]],np.float32)
-dst = np.array([[0,120],[480,360],[450,120],[0,360]],np.float32)
+src = np.array([[0,0],[480,0],[480,360],[0,360]],np.float32)#The orginal image points
+dst = np.array([[0,120],[480,360],[450,120],[0,360]],np.float32)#I don't know exactly what the destination points would be...
 
 M = cv.getPerspectiveTransform(src, dst)
-warp = cv.warpPerspective(img1.copy(), M, (450, 300))
+warp = cv.warpPerspective(img1.copy(), M, (480, 360))
 cv.imshow('image', img1)
 cv.imshow('transform', warp)
 cv.waitKey(0)
