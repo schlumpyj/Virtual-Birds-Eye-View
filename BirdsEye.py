f688994d2d37254818ac63646a0fa3bedd6b4dda
bb20basketball <bb20basketball@gmail.com>
2016-12-12 22:25:53 -0500

    Updated for MJPG-Streamer

--- BirdsEye.py
+++ BirdsEye.py
@@ -1,60 +1,62 @@
 import cv2 as cv
 import numpy as np
-import time
+vid2 = cv.VideoCapture(1)
+vid3 = cv.VideoCapture(2)
+class MyFilter:
+    
+    
+    def process(self, img1):
+        '''
+            :param img: A numpy array representing the input image
+            :returns: A numpy array to send to the mjpg-streamer output plugin
+        '''
+        _, img2 = vid2.read()
+        _, img3 = vid3.read()
+        img1 = cv.resize(img1, (400, 300))
+        img2 = cv.resize(img2, (400, 300))
+        img3 = cv.resize(img3, (400, 300))
+        #img1 = cv.flip(img1, 0)
+        #img1 = cv.flip(img1, 1)
+        #img2 = cv.flip(img2, 0)
+        #img2 = cv.flip(img2, 1)
+        #img3 = cv.flip(img3, 0)
+        #img3 = cv.flip(img3, 1)
+        image = np.zeros((400, 400, 3), np.uint8)
+        image2 = np.zeros((400, 400, 3), np.uint8) 
+        src = np.array([[0,150],[400,150],[400,300],[0,300]],np.float32)
+        dst = np.array([[0,0],[400,0],[250,150],[150,150]],np.float32)
+        ##########################Pushing   ^^ Down to below 200 gives the weird image
 
-vid1 = cv.VideoCapture(0)
-vid2 = cv.VideoCapture("http://10.44.80.61:5800/?action=stream")
-vid1.set(6, cv.VideoWriter_fourcc(*"MJPG"))#Should set the codec, but it doesn't
-vid1.set(10, 50)
-vid1.set(3, 640)
-vid1.set(4, 480)
-vid2.set(6, cv.VideoWriter_fourcc(*"MJPG"))#Should set the codec, but it doesn't
-vid2.set(10, 50)
-vid2.set(3, 640)
-vid2.set(4, 480)
-
-fourcc = cv.VideoWriter_fourcc(*'DIV3')
-out1 = cv.VideoWriter('try.avi',fourcc, 20.0, (640,480))
-out2 = cv.VideoWriter('try2.avi',fourcc, 20.0, (640,480))
-start = time.time()
-counter =0
-while True:
-    _, img1 = vid1.read()
-    _, img2 = vid2.read()
-    out1.write(img1)
-    out2.write(img2)
-    counter+=1
-    img1 = cv.resize(img1, (480, 360))
-    img2 = cv.resize(img2, (480, 360))
-    image = np.zeros((500, 500, 3), np.uint8)
-    image2 = np.zeros((500, 500, 3), np.uint8) 
-    src = np.array([[0,200],[480,200],[480,360],[0,360]],np.float32)
-    dst = np.array([[0,0],[480,0],[300,200],[180,200]],np.float32)
-    ##########################Pushing   ^^ Down to below 200 gives the weird image
-
-    M = cv.getPerspectiveTransform(src, dst)
-    warp = cv.warpPerspective(img1.copy(), M, (480, 360))
+        M = cv.getPerspectiveTransform(src, dst)
+        warp = cv.warpPerspective(img1.copy(), M, (400, 300))
 
 
 
-    two = cv.warpPerspective(img2.copy(), M, (480, 360))
-    two = cv.resize(two,(0,0), fx=2, fy=2)
+        two = cv.warpPerspective(img2.copy(), M, (400, 300))
+        two = cv.resize(two,(0,0), fx=2, fy=2)
 
+        three = cv.warpPerspective(img3.copy(), M, (400, 300))
+        three = cv.resize(three,(0,0), fx=2, fy=2)
+        
+        image2[:warp.shape[0], :warp.shape[1]]= warp
+        twoR = cv.getRotationMatrix2D((two.shape[1]/2,two.shape[0]/2),90,.5)
+        twoD = cv.warpAffine(two,twoR,(two.shape[1],two.shape[0]))
 
-    image2[:warp.shape[0], 15:warp.shape[1]+15]= warp
-    twoR = cv.getRotationMatrix2D((two.shape[1]/2,two.shape[0]/2),90,.5)
-    twoD = cv.warpAffine(two,twoR,(two.shape[1],two.shape[0]))
+        threeR = cv.getRotationMatrix2D((three.shape[1]/2,three.shape[0]/2),180,.5)
+        threeD = cv.warpAffine(three,threeR,(three.shape[1],three.shape[0]))
 
-    twoD = twoD[100:600, 310:500]
+        twoD = twoD[100:500, 250:500]
+        threeD = threeD[200:450, 200:600]
+        image[0:twoD.shape[0]+0, 0:twoD.shape[1]+0]= twoD
+        image2[150:threeD.shape[0]+250, 0:threeD.shape[1]]=threeD
+        ora = cv.add(image, image2)
+        return ora
+        
+def init_filter():
+    '''
+        This function is called after the filter module is imported. It MUST
+        return a callable object (such as a function or bound method). 
+    '''
+    f = MyFilter()
+    return f.process
 
-    image[0:twoD.shape[0]+0, 0:twoD.shape[1]+0]= twoD
-
-    ora = cv.add(image, image2)
-    cv.imshow("image", ora)
-    k = cv.waitKey(5) & 0xFF
-    if k == 27:
-        break
-print counter/(time.time()-start)
-vid1.release()
-vid2.release()
-cv.destroyAllWindows()
