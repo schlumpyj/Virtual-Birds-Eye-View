e1c134f250cbe4faa33fa5a9536bc8af9505140f
bb20basketball <bb20basketball@gmail.com>
2016-12-13 23:51:18 -0500

    Minor CPU improvement
    .6% - .9% CPU improvement

--- BirdsEye.py
+++ BirdsEye.py
@@ -2,13 +2,32 @@ import cv2 as cv
 import numpy as np
 vid2 = cv.VideoCapture(1)
 vid3 = cv.VideoCapture(2)
+
 preImg1 = np.zeros((300, 400, 3), np.uint8)
 preImg2 = np.zeros((300, 400, 3), np.uint8)
 preImg3 = np.zeros((300, 400, 3), np.uint8)
+
 img2 = np.zeros((480, 640, 3), np.uint8)
 img3 = np.zeros((480, 640, 3), np.uint8)
+
+image = np.zeros((400, 400, 3), np.uint8)
+image2 = np.zeros((400, 400, 3), np.uint8)
+
+finalSave = np.zeros((400, 400, 3), np.uint8)
+
+resizeMe = np.zeros((600, 800, 3), np.uint8)
+resizeMe2 = np.zeros((600, 800, 3), np.uint8)
+
+warpMe = np.zeros((300, 400, 3), np.uint8)
+warpMe2 = np.zeros((300, 400, 3), np.uint8)
+warpMe3 = np.zeros((300, 400, 3), np.uint8)
+
+rotation = np.zeros((300, 400, 3), np.uint8)
+rotation2 = np.zeros((300, 400, 3), np.uint8)
+
 src = np.array([[0,150],[400,150],[400,300],[0,300]],np.float32)
 dst = np.array([[0,0],[400,0],[250,150],[150,150]],np.float32)
+
 twoR = cv.getRotationMatrix2D((400,300),90,.5)
 threeR = cv.getRotationMatrix2D((400,300),180,.5)
 
@@ -28,28 +47,27 @@ class MyFilter:
         #img2 = cv.flip(img2, 1)
         #img3 = cv.flip(img3, 0)
         #img3 = cv.flip(img3, 1)
-        image = np.zeros((400, 400, 3), np.uint8)
-        image2 = np.zeros((400, 400, 3), np.uint8) 
 
         M = cv.getPerspectiveTransform(src, dst)
-        warp = cv.warpPerspective(preImg1.view(), M, (400, 300))
-
-        two = cv.warpPerspective(preImg2.view(), M, (400, 300))
-        two = cv.resize(two,(0,0), fx=2, fy=2)
-
-        three = cv.warpPerspective(preImg3.view(), M, (400, 300))
-        three = cv.resize(three,(0,0), fx=2, fy=2)
         
-        image2[:warp.shape[0], :warp.shape[1]]= warp
-        
-        twoD = cv.warpAffine(two,twoR,(two.shape[1],two.shape[0]))
-        threeD = cv.warpAffine(three,threeR,(three.shape[1],three.shape[0]))
+        cv.warpPerspective(preImg1, M, (400, 300), dst=warpMe)
 
-        twoD = twoD[100:500, 250:500]
-        threeD = threeD[200:450, 200:600]
-        image[0:twoD.shape[0]+0, 0:twoD.shape[1]+0]= twoD
-        image2[150:threeD.shape[0]+250, 0:threeD.shape[1]]=threeD
-        ora = cv.add(image, image2)
+        cv.warpPerspective(preImg2, M,(400, 300), dst=warpMe2)
+        cv.resize(warpMe2,(0,0), dst=resizeMe, fx=2, fy=2)
+
+        cv.warpPerspective(preImg3, M,(400, 300), dst=warpMe3)
+        cv.resize(warpMe3,(0,0), dst=resizeMe2, fx=2, fy=2)
+        
+        image2[:warpMe.shape[0], :warpMe.shape[1]]= warpMe
+        
+        rotation = cv.warpAffine(resizeMe,twoR,(resizeMe.shape[1],resizeMe.shape[0]))
+        rotation2 = cv.warpAffine(resizeMe2,threeR,(resizeMe2.shape[1],resizeMe2.shape[0]))#could rid of this .shape stuff
+
+        rotation = rotation[100:500, 250:500]
+        rotation2 = rotation2[200:450, 200:600]
+        image[0:rotation.shape[0]+0, 0:rotation.shape[1]+0]= rotation
+        image2[150:rotation2.shape[0]+250, 0:rotation2.shape[1]]=rotation2
+        ora = cv.add(image, image2, dst=finalSave)
         return ora
         
 def init_filter():
