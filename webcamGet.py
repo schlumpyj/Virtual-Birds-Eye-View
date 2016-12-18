75f55da2dfa865df755fb43dd6b5162386c7be4d
Joshua <bb20basketball@gmail.com>
2016-12-17 23:46:24 -0500

    Create webcamGet.py

---
+++ webcamGet.py
@@ -0,0 +1,35 @@
+from threading import Thread
+import cv2
+ 
+class WebcamVideoStream:
+    
+    def __init__(self, src=0):
+
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+   
+   def start(self):
+
+        Thread(target=self.update, args=()).start()
+        return self
+ 
+    def update(self):
+
+        while True:
+                
+            if self.stopped:
+                self.stream.release()
+                return
+
+                
+            (self.grabbed, self.frame) = self.stream.read()
+		
+
+    def read(self):
+            
+        return self.frame
+
+    def stop(self):
+            
+        self.stopped = True
