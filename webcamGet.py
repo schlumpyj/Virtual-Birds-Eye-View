6a23e100cbc3ac26255c6e86deec738ce58e1e85
Joshua <bb20basketball@gmail.com>
2016-12-20 20:28:03 -0500

    Added Camera Recording

--- webcamGet.py
+++ webcamGet.py
@@ -1,30 +1,39 @@
 from threading import Thread
+import time
 import cv2
  
 class WebcamVideoStream:
     
-    def __init__(self, src=0):
+    def __init__(self, camera, src=0):
 
         self.stream = cv2.VideoCapture(src)
         (self.grabbed, self.frame) = self.stream.read()
+        if camera == 1:
+            fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+            self.out = cv2.VideoWriter('camera1.avi', fourcc, 10.0, (320,240))
+        else:
+            fourcc = cv2.VideoWriter_fourcc(*'DIV3')
+            self.out = cv2.VideoWriter('camera'+str(camera)+'.avi',fourcc, 10.0, (640,480))
         self.stopped = False
-   
-   def start(self):
+    def start(self):
 
         Thread(target=self.update, args=()).start()
         return self
  
     def update(self):
-
-        while True:
+	
+        while True:	
             if self.stopped:
                 self.stream.release()
-                return
+                return   
             (self.grabbed, self.frame) = self.stream.read()
-		
+            self.out.write(self.frame)
+
 
     def read(self):
+            
         return self.frame
 
     def stop(self):
+
         self.stopped = True
